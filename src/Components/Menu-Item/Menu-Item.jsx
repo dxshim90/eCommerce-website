7fbde44bc2a6142fbc758046a756c9463ece0738
dxshim90 <dxshim90@gmail.com>
2019-07-23 10:23:23 +0100

    homepage broken into components and styles aded to imsages

--- src/Components/Menu-Item/Menu-Item.jsx
+++ src/Components/Menu-Item/Menu-Item.jsx
@@ -4,12 +4,13 @@ import "./Menu-Item.scss";
 
 const MenuItem = ({ title, imageURL, size }) => {
   return (
-    <div
-      style={{ backgroundImage: `url(${imageURL})` }}
-      className={`${size} menu-item`}
-    >
+    <div className={`${size} menu-item`}>
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageURL})` }}
+      />
       <div className="content">
-        <h1 className="title">{title}</h1>
+        <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">Shop Now</span>
       </div>
     </div>
